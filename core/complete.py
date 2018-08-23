e7205c9d5e865dfbf6b8c7c93fcd0076ba0b8305
Debby Anggraini <Xnver404@gmail.com>
2018-08-23 08:47:53 +0700

    Add files via upload

---
+++ core/complete.py
@@ -0,0 +1,26 @@
+##
+## auto complete
+##
+## Credit to (http://stackoverflow.com/a/187660)
+##
+
+import readline
+array = ["help",  "quit"]
+class auto(object):
+    def __init__(self, options):
+        self.options = sorted(options)
+    def complete(self, text, state):
+        if state == 0:
+            if text:
+                self.matches = [s for s in self.options
+                                    if s and s.startswith(text)]
+            else:
+                self.matches = self.options[:]
+        try:
+            return self.matches[state]
+        except IndexError:
+            return None
+def complete(array):
+	completer = auto(array)
+	readline.set_completer(completer.complete)
+	readline.parse_and_bind('tab:complete')
